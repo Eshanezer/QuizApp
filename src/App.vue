18a27139b59f84adc0e37e2af6c5e1dde2a9d643
Eshan Dilmina <106302589+eshanceylonsoft@users.noreply.github.com>
2023-04-29 18:08:04 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<script setup>
+import q from "./data/quizes.json";
+import {ref,watch} from "vue";
+
+const quizes =ref(q);
+const search = ref("");
+
+watch(search,()=>{
+  
+});
+
+</script>
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input v-model="search" type="text" placeholder="Search...">
+    </header>
+    <div class="option-container">
+      <div class="card" v-for="quiz in quizes" :key="quiz.id">
+        <img :src="quiz.img" alt="">
+        <div class="card-text">
+          <h2>{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.length }} Questions</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  max-width: 1000px;
+  margin: 0 auto;
+}
+header h1{
+  font-weight: bold;
+  margin-right: 30px;
+}
+.option-container{
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 4px;
+}
+header input{
+  border: none;
+  background-color: rgba(128,128,128,0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.card{
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgab(0,0,0,0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+.card img{
+  width: 100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card .card-text{
+  padding: 0 5px;
+}
+.card .card-text h2{
+  font-weight: bold;
+}
+</style>
